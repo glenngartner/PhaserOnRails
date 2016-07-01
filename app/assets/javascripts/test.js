986807d9582c8caec2d71455b4ec3193d79eacd7
Glenn Gartner <glenngartner@gmail.com>
2016-07-01 00:26:09 -0400

    Add round rectangle shapes

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -1,44 +1,28 @@
 var game = new Phaser.Game(innerWidth, innerHeight, Phaser.CANVAS, 'phaser-example', { create: create, update: update });
 
-var bmd;
-var circle;
-
-var colors;
-var i = 0;
-var p = null;
+var circle, roundRec;
 
 function create() {
 
-    colors = Phaser.Color.HSVColorWheel();
-
     //  Create a Circle
-    circle = new Phaser.Circle(game.world.centerX, game.world.centerY, 500);
+    // circle = new Phaser.Circle(game.world.centerX, game.world.centerY, 500);
+    recWidth = 100;
+    // roundRec = new Phaser.RoundedRectangle(game.world.centerX-recWidth/2, game.world.centerY, recWidth,recWidth,10);
+    // roundRec.inputEnabled = true;
+    // roundRec.input.enableDrag(true);
 
-    //  Create a BitmapData just to plot Circle points to
-    bmd = game.add.bitmapData(game.width, game.height);
-    bmd.addToWorld();
-
-    //  And display our circle on the top
+     //  And display our circle on the top
     var graphics = game.add.graphics(0, 0);
+    graphics.beginFill(0xFF0000);
     graphics.lineStyle(1, 0x00ff00, 1);
-    graphics.drawCircle(circle.x, circle.y, circle.diameter);
+    // graphics.drawCircle(circle.x, circle.y, circle.diameter);
 
-    p = new Phaser.Point();
+    graphics.drawRoundedRect(game.world.centerX-recWidth/2, game.world.centerY, recWidth, recWidth, 10);
 
 }
 
 function update () {
 
-    for (var c = 0; c < 10; c++)
-    {
-        circle.random(p);
 
-        //  We'll floor it as setPixel needs integer values and random returns floats
-        p.floor();
-
-        bmd.setPixel(p.x, p.y, colors[i].r, colors[i].g, colors[i].b);
-    }
-
-    i = game.math.wrapValue(i, 1, 359);
 
 }
