9445c19ee9b4ffd880f74b9e8b2eb45f412a2b9c
Glenn Gartner <glenngartner@gmail.com>
2016-07-07 23:37:42 -0400

    Add views, bootstrap, render partial

--- Gemfile
+++ Gemfile
@@ -45,3 +45,6 @@ group :development do
   gem 'spring'
 end
 
+
+# install bootstrap sass
+gem 'bootstrap-sass'
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   get 'game/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'game#index'
+  root 'welcome#index'
   get 'game/edit'
 
   # Example of regular route:
