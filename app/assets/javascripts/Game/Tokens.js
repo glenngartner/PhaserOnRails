b699d457f4cb8130790e5beff8388fa3fec622d1
Glenn Gartner <glenngartner@gmail.com>
2016-07-09 11:23:16 -0400

    remove assignment to scale arguments in newDraggableToken function

--- app/assets/javascripts/Game/Tokens.js
+++ app/assets/javascripts/Game/Tokens.js
@@ -4,12 +4,11 @@
 
 
 // Create a new token that's draggable, with variable size options
-function newDraggableToken(name, x, y, preload_string_ref, scalex = 1 , scaley =1 ){
+function newDraggableToken(name, x, y, preload_string_ref, scalex, scaley){
     name = game.add.sprite(x,y, preload_string_ref);
     name.scale.setTo(scalex, scaley);
     name.inputEnabled = true;
     name.input.enableDrag();
-    // name.input.onInputUp.add(consoleLog("dropped the piece!"), name);
 
     return name;
 };
