2648ac9fc51ee5332e2ab69955281fb24f8621dd
Glenn Gartner <glenngartner@gmail.com>
2016-07-09 03:49:13 -0400

    added heart position tracker on the board, and some JSON prep. One step closer to persistence with a db

--- app/assets/javascripts/Game/Tokens.js
+++ app/assets/javascripts/Game/Tokens.js
@@ -2,10 +2,14 @@
  * Created by g on 7/9/16.
  */
 
+
+// Create a new token that's draggable, with variable size options
 function newDraggableToken(name, x, y, preload_string_ref, scalex = 1 , scaley =1 ){
     name = game.add.sprite(x,y, preload_string_ref);
     name.scale.setTo(scalex, scaley);
     name.inputEnabled = true;
     name.input.enableDrag();
+    // name.input.onInputUp.add(consoleLog("dropped the piece!"), name);
+
     return name;
 };
